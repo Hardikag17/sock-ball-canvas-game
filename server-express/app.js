1c43386a1bccf140f27a36560210eecc58d378c4
hardikag17 <hardikag17@gmail.com>
2024-03-08 01:29:27 +0530

    fix server logic

--- server-express/app.js
+++ server-express/app.js
@@ -9,26 +9,30 @@ const app = express();
 
 const server = http.createServer(app);
 // clientHost -> Screen Number
-const clientsList = new Map();
-// Ws-> clientHost
-const connections = new Map();
-
+const clientsList = {};
+let clientsListAsArray = [];
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function (ws, req) {
     const clientUniqID = req?.url?.slice(req?.url?.indexOf('=') + 1);
-    const clientHost = clientUniqID;
     console.log("clientHost", clientUniqID);
     let screenNumForClient = -1;
-    if (typeof clientHost === 'string' && clientHost.trim().length > 0) {
-        if (clientsList.has(clientHost)) {
-            screenNumForClient = clientsList.get(clientHost);
-            // console.log('SCREEN ALREADY FOUND RETURNING SAME SCREEN', clientHost, screenNumForClient);
+    if (typeof clientUniqID === 'string' && clientUniqID.trim().length > 0) {
+        if (clientsList[clientUniqID]) {
+            screenNumForClient = clientsList[clientUniqID]?.screenNumber;
+            // console.log('SCREEN ALREADY FOUND RETURNING SAME SCREEN', clientUniqID, screenNumForClient);
         } else {
-            screenNumForClient = clientsList.size + 1;
-            clientsList.set(clientHost, screenNumForClient);
-            connections[ws] = clientHost;
+            screenNumForClient = Object.keys(clientsList).length + 1;
         }
+
+        clientsList[clientUniqID] = {
+            ws: ws,
+            screenNumber: screenNumForClient,
+            clientUniqID: clientUniqID
+        };
+
+        clientsListAsArray = [...Object.keys(clientsList)?.map(e => clientsList?.[e])];
+        console.warn(clientsList);
     }
 
     let withBall = 0;
@@ -40,23 +44,73 @@ wss.on('connection', function (ws, req) {
     broadcast({ type: 'online', data: online });
 
     ws.on('message', function (data) {
-        console.log(JSON.parse(data).data);
-        const values = JSON.parse(data).data;
-        const index = [...wss.clients].indexOf(ws);
+        const parsedData = JSON.parse(data);
+        console.log(parsedData);
+        const values = parsedData?.data;
+        const clientUniqueID = values?.[4];
+        const currentClientScreenNum = clientsList[clientUniqueID]?.screenNumber;
+        const currentClientObj = clientsList?.[clientUniqueID];
+        let targetClientID = null;
 
-        if (values[0] == 0) {
-            withBall = [...wss.clients][index + 1] ? index + 1 : 0;
+
+        console.error(clientUniqueID);
+        // If values[0] === 0 then ball is sent backwards, and 1 if it's next screen.
+        const isBallSentBackwardsToPrevScreen = values[0] === 0;
+
+        // const index = [...wss.clients].indexOf(ws);
+        let targetScreenOfBall = 0;
+
+        if (isBallSentBackwardsToPrevScreen) {
+            const res = clientsListAsArray?.find(e => e?.screenNumber === currentClientScreenNum - 1) ?? clientsListAsArray?.[clientsListAsArray?.length - 1];
+            targetClientID = res?.clientUniqID;
+            targetScreenOfBall = res?.screenNumber;
+
+            // for (const clientID in clientsList) {
+            //     if (clientsList[clientID].screenNumber === currentClientScreenNum) {
+            //         targetClientID = clientID;
+            //         targetScreenOfBall = clientsList[targetClientID].screenNumber;
+            //         break;
+            //     }
+            // }
+
+            console.warn('previousScreenClientID', targetClientID, targetScreenOfBall);
+
+
+
+
+            // withBall = [...wss.clients][index + 1] ? index + 1 : 0;
         } else {
-            withBall = [...wss.clients][index - 1] ? index - 1 : [...wss.clients].length - 1;
+            const res = clientsListAsArray?.find(e => e?.screenNumber === currentClientScreenNum + 1) ?? clientsListAsArray?.[0];
+            targetClientID = res?.clientUniqID;
+            targetScreenOfBall = res?.screenNumber;
+
+            console.error(clientsListAsArray, currentClientScreenNum, targetClientID, targetScreenOfBall)
+
+
+            // for (const clientID in clientsList) {
+            //     if (clientsList[clientID].screenNumber - 1 === currentClientScreenNum) {
+            //         targetClientID = clientID;
+            //         targetScreenOfBall = clientsList[targetClientID].screenNumber;
+            //         break;
+            //     }
+            // }
+
+            console.warn('nextScreenClientID', targetClientID, targetScreenOfBall);
+
+
+            // withBall = [...wss.clients][index - 1] ? index - 1 : [...wss.clients].length - 1;
         }
-        let user_connection = connections[[...wss.clients][withBall]];
-        withBall_screen_numebr = clientsList.get(user_connection);
-        console.log("user_connection, withBall_screen_numebr: ", user_connection, withBall_screen_numebr);
-        [...wss.clients][withBall].send(JSON.stringify({ type: 'ball.get', data: values }));
-        broadcast({ type: 'withBall', data: withBall_screen_numebr }); // Current ball screen status
+
+        // let user_connection = connections[[...wss.clients][withBall]];
+        // withBall_screen_numebr = clientsList.get(user_connection);
+        // console.log("user_connection, withBall_screen_numebr: ", user_connection, withBall_screen_numebr);
+        clientsList[targetClientID].ws.send(JSON.stringify({ type: 'ball.get', data: values }));
+        // [...wss.clients][withBall].send();
+        broadcast({ type: 'withBall', data: clientsList[targetClientID].screenNumber }); // Current ball screen status
     });
 
-    ws.on('close', function () {
+    ws.on('close', function (data) {
+        console.warn(data);
         online = wss.clients.size;
         broadcast({ type: 'online', data: online });
     });
