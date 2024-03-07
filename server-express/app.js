345b179a53ca89edaafaf3b2f7ea65b7e1c51cfe
hardikag17 <hardikag17@gmail.com>
2024-03-08 00:14:19 +0530

    user connections logic

--- server-express/app.js
+++ server-express/app.js
@@ -8,20 +8,37 @@ const app = express();
 // app.use(express.static(path.join(__dirname, 'client')));
 
 const server = http.createServer(app);
+// clientHost -> Screen Number
+const clientsList = new Map();
+// Ws-> clientHost
+const connections = new Map();
 
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', function (ws) {
-    let withBall = 0;
-    console.log("withBall", withBall)
-    let online = wss.clients.size;
-    console.log("online", online)
+wss.on('connection', function (ws, req) {
+    const clientHost = req.headers.host;
+    let screenNumForClient = -1;
+    if (typeof clientHost === 'string' && clientHost.trim().length > 0) {
+        if (clientsList.has(clientHost)) {
+            screenNumForClient = clientsList.get(clientHost);
+            // console.log('SCREEN ALREADY FOUND RETURNING SAME SCREEN', clientHost, screenNumForClient);
+        } else {
+            screenNumForClient = clientsList.size + 1;
+            clientsList.set(clientHost, screenNumForClient);
+            connections[ws] = clientHost;
+        }
+    }
 
-    let screenNumber = wss.clients.size - 1;
-    ws.send(JSON.stringify({ type: 'screenNumber', data: screenNumber }));
+    let withBall = 0;
+    let online = wss.clients.size;
+
+    ws.send(JSON.stringify({ type: 'screenNumber', data: screenNumForClient }));
+    // console.log("CLIENT ADDED OK", req.headers.host, screenNumForClient);
+
     broadcast({ type: 'online', data: online });
 
     ws.on('message', function (data) {
+        console.log(JSON.parse(data).data);
         const values = JSON.parse(data).data;
         const index = [...wss.clients].indexOf(ws);
 
@@ -30,9 +47,11 @@ wss.on('connection', function (ws) {
         } else {
             withBall = [...wss.clients][index - 1] ? index - 1 : [...wss.clients].length - 1;
         }
-
+        let user_connection = connections[[...wss.clients][withBall]];
+        withBall_screen_numebr = clientsList.get(user_connection);
+        console.log("user_connection, withBall_screen_numebr: ", user_connection, withBall_screen_numebr);
         [...wss.clients][withBall].send(JSON.stringify({ type: 'ball.get', data: values }));
-        broadcast({ type: 'withBall', data: withBall }); // Current ball screen status
+        broadcast({ type: 'withBall', data: withBall_screen_numebr }); // Current ball screen status
     });
 
     ws.on('close', function () {
